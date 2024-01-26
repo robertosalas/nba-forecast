f5440907c9e05b46d8b089e415dfe12962e22d74
Roberto <jrsalas@outlook.com>
2024-01-26 14:06:13 +0100

    changes

--- src/Train-Models/XGBoost_Model_ML.py
+++ src/Train-Models/XGBoost_Model_ML.py
@@ -48,4 +48,4 @@ for x in tqdm(range(300)):
     acc_results.append(acc)
     # only save results if they are the best so far
     if acc == max(acc_results):
-        model.save_model('./Models/XGBoost_{}%_ML-4.json'.format(acc))
+        model.save_model('./Models/XGBoost_{}%_ML-3.json'.format(acc))
